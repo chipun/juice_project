8f435ec2e085bb90f6defc072d5e95b06e37a28c
cpun <pun_chiming@bah.com>
2016-04-27 16:51:25 -0400

    add new pages

---
+++ scripts/app.js
@@ -0,0 +1,21 @@
+var juiceApp = angular.module("juiceApp",['ngRoute']);
+
+juiceApp.controller("recipeController",function($http){
+	this.recipes = {'Adios Coffee', 'Apple Crisp','Appleberry Lush','Asparagus Delight ','Beets and Treats'};
+});
+
+juiceApp.controller("healthController",function($http){
+	this.healthdata = {'Alzheimer''s Prevention', 'Antioxidants','Cancer Prevention','Arthritis','Breast Cancer'};
+});
+
+juiceApp.controller("benefitController",function($http){
+	this.benefits = {'carrot', 'orange','grape','kiwi','ginger'};
+});
+
+
+juiceApp.controller("ingredientController",function($http){
+	this.ingredients = {'carrot', 'orange','grape','kiwi','ginger'};
+});
+
+
+
---
+++ scripts/route.js
@@ -0,0 +1,35 @@
+angular.module('juiceApp').config(function($routeProvider) {
+    $routeProvider.when('/', {
+            templateUrl: 'templates/content-index.html'
+        })
+        .when('/benefits', {
+            templateUrl: 'templates/pages/benefits/benefits-index.html',
+            controller: 'benefitsController',
+            controllerAs: 'benCtrl'
+        })
+        .when('/health', {
+            templateUrl: 'templates/pages/health/health-index.html',
+            controller: 'healthController',
+            controllerAs: 'healthCtrl'
+        })
+        .when('/ingredients', {
+            templateUrl: 'templates/pages/ingredients/ingredients-index.html',
+            controller: 'ingredientsController',
+            controllerAs: 'ingCtrl'
+        })
+        .when('/recipes', {
+            templateUrl: 'templates/pages/recipes/recipes-index.html',
+            controller: 'recipesController',
+            controllerAs: 'recipesCtrl'
+        })
+        .when('/about', {
+            templateUrl: 'templates/about.html'
+          
+        })
+        .when('/contact', {
+            templateUrl: 'templates/contact.html'
+            
+        })
+
+    .otherwise({ redirectTo: '/' });
+})
