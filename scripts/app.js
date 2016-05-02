45363f637bd429ae3c2b5b767aa0703ca4e2b867
chipun <chipun@gmail.com>
2016-05-02 10:49:19 -0400

    Updated app.js and added ng-repeat directive to each page that will display a list

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 var juiceApp = angular.module("juiceApp",['ngRoute']);
 
-juiceApp.controller("recipeController",function($http){
+juiceApp.controller("recipesController",function($http){
 	this.recipes = ['Adios Coffee', 'Apple Crisp','Appleberry Lush','Asparagus Delight ','Beets and Treats'];
 });
 
@@ -8,12 +8,12 @@ juiceApp.controller("healthController",function($http){
 	this.healthdata = ['Alzheimer"s Prevention', 'Antioxidants','Cancer Prevention','Arthritis','Breast Cancer'];
 });
 
-juiceApp.controller("benefitController",function($http){
+juiceApp.controller("benefitsController",function($http){
 	this.benefits = ['carrot', 'orange','grape','kiwi','ginger'];
 });
 
 
-juiceApp.controller("ingredientController",function($http){
+juiceApp.controller("ingredientsController",function($http){
 	this.ingredients = ['carrot', 'orange','grape','kiwi','ginger'];
 });
 
