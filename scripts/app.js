124ba377e8099669335a52ab39e516c4e452d3b7
chipun <chipun@gmail.com>
2016-05-06 10:33:50 -0400

    Created a new factory for API

--- scripts/app.js
+++ scripts/app.js
@@ -1,52 +1,53 @@
 var juiceApp = angular.module("juiceApp", ['ngRoute', 'ngMessages']);
 
-var globalurlJSON = "/juice_project/juiceDataJSON.json";
+var globalurlJSON = "/juiceDataJSON.json";
+var list;
 
-juiceApp.controller("recipesController", function($http,$routeParams) {
+juiceApp.controller("recipesController", function($http, $scope, $routeParams, apiFactory) {
 
-  var controller = this;
+    var controller = this;
 
-    $http({method:"GET", url:globalurlJSON, headers:{'Content-Type':'application/json'}}).success(function(data){
-        var list = data.recipesData.recipes;
-        controller.recipes = list;
+    apiFactory.getData().then(function(data) {
+        controller.recipes = data.recipesData.recipes;
     });
 
 });
 
-juiceApp.controller("healthController", function($http,$routeParams) {
-	
+juiceApp.controller("healthController", function($http, $scope, $routeParams, apiFactory) {
+
     var controller = this;
 
-	$http({method:"GET", url:globalurlJSON, headers:{'Content-Type':'application/json'}}).success(function(data){
-		var list = data.healthData.healths;
-		controller.healthdata = list;
-	});
+    apiFactory.getData().then(function(data) {
+        controller.healthdata = data.healthData.healths;
+    });
+});
+
+juiceApp.controller("benefitsController", function($http, $scope, $routeParams, apiFactory) {
+    var controller = this;
+
+    if ($routeParams.name) {
+        $scope.detailname = $routeParams.name
+    } else {
+
+        apiFactory.getDataBy('benefits').then(function(data) {
+            controller.benefits = data;
+        });
+    }
 
 });
 
-juiceApp.controller("benefitsController", function($http,$routeParams) {
-	var controller = this;
 
-	$http({method:"GET", url:globalurlJSON, headers:{'Content-Type':'application/json'}}).success(function(data){
-		var list = data.vegData.vegetables;
-		controller.benefits = list;
-	});
+juiceApp.controller("ingredientsController", function($http, $scope, $routeParams, apiFactory) {
 
-});
+    var controller = this;
 
-
-juiceApp.controller("ingredientsController", function($http, $routeParams) {
-
-	var controller = this;
-
-	$http({method:"GET", url:globalurlJSON, headers:{'Content-Type':'application/json'}}).success(function(data){
-		var list = data.fruitData.fruits;
-		controller.ingredients = list;
-	});
+    apiFactory.getData().then(function(data) {
+        controller.ingredients = data.fruitData.fruits;
+    });
 
 });
 
-juiceApp.controller("contactController", ['$http','$scope', '$window', function($http, $scope, $window) {
+juiceApp.controller("contactController", ['$http', '$scope', '$window', function($http, $scope, $window) {
 
     $scope.submitClick = function() {
 
@@ -66,8 +67,58 @@ juiceApp.controller("contactController", ['$http','$scope', '$window', function(
             fname: '',
             lname: '',
             email: '',
-            messages:''
+            messages: ''
         };
     };
 
 }]);
+
+
+juiceApp.factory('apiFactory', function($http, $location, $q) {
+
+    var _baseAbsUrl = $location.absUrl();
+    var _baseURL = "/juice_project/juiceDataJSON.json";
+
+
+    var services = {}
+
+    services.getData = function() {
+
+        var deferred = $q.defer();
+
+        $http({ method: "GET", url: _baseURL, headers: { 'Content-Type': 'application/json' } }).success(function(data) {
+            deferred.resolve(data);
+        });
+
+        return deferred.promise;
+    }
+
+    services.getDataBy = function(category) {
+
+        var deferred = $q.defer();
+
+        $http({ method: "GET", url: _baseURL, headers: { 'Content-Type': 'application/json' } }).success(function(data) {
+
+            switch (category) {
+                case 'ingredients':
+                    deferred.resolve(data.fruitData.fruits);
+                    break;
+                case 'benefits':
+                    deferred.resolve(data.vegData.vegetables);
+                    break;
+                case 'health':
+                    deferred.resolve(data.healthData.healths);
+                    break;
+                case 'recipes':
+                    deferred.resolve(data.recipesData.recipes);
+                    break;
+            }
+
+        });
+
+        return deferred.promise;
+    }
+
+    return services;
+
+});
